ce888c9d859458ab4a2302d31f8719d5f474f69a
Carles Mateu <carlesm@carlesm.com>
2023-02-19 12:21:59 +0000

    Created empty python script

---
+++ WebScrape/basicclient.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# vim: set fileencoding=utf8 :
+'''
+Simple web client, using URLLIB and basic parsing
+
+@author: carlesm
+'''
+
+
+
+
+if __name__ == "__main__":
+    # create client class
+    # get data from client
+    # print data
+    pass
