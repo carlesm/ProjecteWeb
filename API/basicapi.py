7b50b0f6d95191fe78a1d5348a864a09715091e8
Carles Mateu <carlesm@carlesm.com>
2023-02-19 17:56:11 +0000

    Skeleton of a query

--- API/basicapi.py
+++ API/basicapi.py
@@ -9,11 +9,22 @@ https://info.arxiv.org/help/api/basics.html
 @author: carlesm
 '''
 
+class ArxivClient(object):
+    def def __init__(self):
+        super(ArxivClient, self).__init__()
+        self.url = "http://export.arxiv.org/api/query?start=0&max_results=1&search_query="
 
+    
+    def get_data(self, query):
+        # query arxiv
+        # convert XML to data
+        # trim data
+        # return data
+        pass    
 
 
 
 if __name__=="__main__":
     client = ArxivClient()
-    data = client.get_data()
+    data = client.get_data("ltsm")
     print(data)
