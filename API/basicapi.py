b6d59df5d66cc775be722fe8e9cca692161b474a
Carles Mateu <carlesm@carlesm.com>
2023-02-19 17:58:26 +0000

    Skeleton of program

--- API/basicapi.py
+++ API/basicapi.py
@@ -14,13 +14,28 @@ class ArxivClient(object):
         super(ArxivClient, self).__init__()
         self.url = "http://export.arxiv.org/api/query?start=0&max_results=1&search_query="
 
+
+    
+    def query_arxiv(self, query):
+        pass
+
+    def xml_to_dict(self, results):
+        pass
+
+    def extract_data(self, data):
+        pass
+
     
     def get_data(self, query):
         # query arxiv
+        results = self.query_arxiv(query)
         # convert XML to data
+        data = self.xml_to_dict(results)
         # trim data
+        data = self.extract_data(data)
         # return data
-        pass    
+        return data
+        
 
 
 
