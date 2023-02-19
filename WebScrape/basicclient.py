fe70941b0820ce09ff0611fa1ba0dd416a030f17
Carles Mateu <carlesm@carlesm.com>
2023-02-19 12:37:51 +0000

    Got the page

--- WebScrape/basicclient.py
+++ WebScrape/basicclient.py
@@ -6,16 +6,24 @@ Simple web client, using URLLIB and basic parsing
 
 @author: carlesm
 '''
-
+import urllib3
 
 class WebClient(object):
 
     def __init__(self) -> None:
         super(WebClient, self).__init__()
         self.data = None
+        # We are going to parse this page, to improve our code, this should be a parameter
+        # in the constructor, or the language should be, f. i.
+        self.url = "https://www.99-bottles-of-beer.net/language-python-808.html"
 
     def get_web_page(self):
         # connect to url
+        httppool = urllib3.PoolManager()
+        result = httppool.request("GET", self.url)
+        print(result.status)
+        print(result.headers)
+        print(result.data)
         # get URL content
         # store content
         pass
