a8988685b17f47edab0d89b9e9e7b6cefbf5ba1e
Carles Mateu <carlesm@carlesm.com>
2023-03-04 11:34:28 +0100

    Added whitenoise

--- DjangoApp/eazytapa/eazytapa/settings.py
+++ DjangoApp/eazytapa/eazytapa/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -117,6 +118,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATIC_ROOT = 'static/'
 
 
 # Default primary key field type
