656ded6a575fdd5038980a360a8f464d842557ed
Carles Mateu <carlesm@carlesm.com>
2023-03-04 11:37:41 +0100

    Added whitenoise static compression

--- DjangoApp/eazytapa/eazytapa/settings.py
+++ DjangoApp/eazytapa/eazytapa/settings.py
@@ -121,6 +121,8 @@ STATIC_URL = 'static/'
 
 STATIC_ROOT = 'static/'
 
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'  
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
